75faf433699396e089b68b6d13b89c796635f153
Kim-geun-woo <128760363+Kim-geun-woo@users.noreply.github.com>
2023-03-31 23:22:56 +0900

    아날로그 초음파센서

---
+++ 센서제어하기/03. 아날로그 초음파(ultrasound) 센서/아날로그 초음파센서.ino
@@ -0,0 +1,23 @@
+int trigPin = 2; int echoPin = 3;
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);  // 출력 핀으로 설정
+  pinMode(echoPin, INPUT);  // 신호를 받는 핀 입력 설정
+}
+
+void loop() 
+{
+  float duration, distance;
+  digitalWrite(trigPin, HIGH);   delay(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = ((float)(340 * duration) / 10000) / 2;
+  Serial.print("Duration:");     
+  Serial.print(duration);
+  Serial.print("\nDistance:");  
+  Serial.print(distance);
+  Serial.println("cm\n");       
+  delay(500);
+}
